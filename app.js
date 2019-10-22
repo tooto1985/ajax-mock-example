0fbc6360412692e0b4859a440117df3421c99df2
Tommy <tooto1985@gmail.com>
2019-10-22 23:45:03 +0800

    modify

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./public'));
+app.get('/getData', function(req, res) {
+    res.json({username: req.query.name, age: 30})
+})
+app.listen(3000);
